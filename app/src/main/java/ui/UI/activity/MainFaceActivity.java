68d9d976d982b7432d38f7222a3891bf47a46728
zhhzhhzhh937 <13306043125@163.com>
2022-03-10 22:46:08 +0800

    第一次提交

--- app/src/main/java/ui/UI/activity/MainFaceActivity.java
+++ app/src/main/java/ui/UI/activity/MainFaceActivity.java
@@ -1,12 +1,25 @@
 package ui.UI.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 import zhh.mvpchatroom.R;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainFaceActivity extends FragmentActivity {
+
+    private ViewPager mViewPager;
+    private FragmentPagerAdapter mAdapter;
+    private List<Fragment> mFragments = new ArrayList<Fragment>();
 
 
 
